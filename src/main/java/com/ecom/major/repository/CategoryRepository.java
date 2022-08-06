21ef006e5e56a18bb81f955bd63bdb4b2a4fb453
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-08-07 02:15:37 +0530

    Update CategoryRepository.java

--- src/main/java/com/ecom/major/repository/CategoryRepository.java
+++ src/main/java/com/ecom/major/repository/CategoryRepository.java
@@ -1,2 +1,7 @@
-package com.ecom.major.repository;public interface CategoryRepository {
+package com.ecom.major.repository;
+
+import com.ecom.major.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
 }
