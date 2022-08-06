f2a99cce66427c707d6ca30a5b41914baa6f9a86
Aditya Sharma <82082352+adityassharma-ss@users.noreply.github.com>
2022-08-07 02:15:53 +0530

    Update ProductReposiory.java

--- src/main/java/com/ecom/major/repository/ProductReposiory.java
+++ src/main/java/com/ecom/major/repository/ProductReposiory.java
@@ -1,4 +1,10 @@
 package com.ecom.major.repository;
 
-public interface ProductService {
+import com.ecom.major.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductReposiory extends JpaRepository<Product, Long> {
+    List<Product> findAllByCategory_Id(int id);
 }
